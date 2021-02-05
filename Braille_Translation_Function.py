367d39394c91be7c5dfcce171d144f2346d4f997
H L <heraklesli@gmail.com>
2021-02-04 17:26:00 -0800

    Add files via upload

---
+++ Braille_Translation_Function.py
@@ -0,0 +1,49 @@
+#Braille Translation
+#This code takes a string and translates it into braille. The function only takes letters and spaces.
+
+brailledict = {
+    'a':'100000',
+    'b':'110000',
+    'c':'100100',
+    'd':'100110',
+    'e':'100010',
+    'f':'110100',
+    'g':'110110',
+    'h':'110010',
+    'i':'010100',
+    'j':'010110',
+    'k':'101000',
+    'l':'111000',
+    'm':'101100',
+    'n':'101110',
+    'o':'101010',
+    'p':'111100',
+    'q':'111110',
+    'r':'111010',
+    's':'011100',
+    't':'011110',
+    'u':'101001',
+    'v':'111001',
+    'w':'010111',
+    'x':'101101',
+    'y':'101111',
+    'z':'101011',
+    ' ':'000000'
+    }
+
+
+def solution(plaintext):
+    textlist = list(plaintext)
+    brlist = []
+    for char in textlist:
+        br = None
+        if  char.isupper() is True:
+            br = char.lower()
+            brlist.append('000001')
+            brlist.append(brailledict[br])
+        else:
+            br = char
+            brlist.append(brailledict[br])
+
+    print(''.join(brlist))
+    return ''.join(brlist)
